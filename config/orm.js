41f553f11ead1c3b0b5b4ef0e84db1f28e511f73
Michael Hrivnak <mhrivnak@gmail.com>
2019-12-18 19:39:51 -0500

    added orm and burger.js code

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,38 @@
 
 // Here is the O.R.M. where you write functions that takes inputs and conditions
 // and turns them into database commands like SQL.
+const connection = require('./connection');
+
+const orm = {
+    selectAll: function(table){
+        let result;
+        let query = "select * from ?";
+
+        connection.query(query,[table], (err,res)=>{
+            if (err) throw err;
+            result = res;
+        });
+        return result;
+    },
+    insertOne: function(table, column, value){
+        let result;
+        let query = "INSERT INTO ? (?) VALUES ?"
+        connection.query(query,[table,column,value], (err,res)=>{
+            if (err) throw err;
+            result = res.id;
+        });
+        return result;
+    },
+    updateOne: function(table, column,value,id){
+        let result;
+        let query = "UPDATE ? SET ? = ? where id = ?"
+        connection.query(query,[table,column,value,id], (err,res)=>{
+            if (err) throw err;
+            result = res.id;
+        });
+        return result;
+    }
+
+}
+
+module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,24 @@
 // your model should do CRUD operations
+const orm = require("../config/orm");
+
+const burger = {
+
+    newBurger: function(id,name,devoured){
+        return {id,name,devoured};
+    },
+    
+    getALLBurgers: function(){
+        return orm.selectAll("burgers");
+    },
+
+    addBurger: function(burgerName){
+        return orm.insertOne("burgers","burger_name",burgerName);
+    },
+
+    eatBurger: function(burgerID){
+        return orm.updateOne("burgers","devoured","1",burgerID);
+    }
+
+};
+
+module.exports = burger;
