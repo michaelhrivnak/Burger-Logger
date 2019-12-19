1262288c0cef7ac487ac6c4523633771ffacc34a
Michael Hrivnak <mhrivnak@gmail.com>
2019-12-18 20:09:10 -0500

    burger controller and server code

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,1 +1,28 @@
 // placeholder for controller code
+const express = require('express');
+const burger = require('../models/burger');
+
+module.exports = function(app){
+
+    app.get("/",(req,res)=>{
+        app.render("index",{
+            burgers: burger.getALLBurgers() 
+        });
+    });
+
+    app.get("/api/all",(req,res)=>{
+        res.json(burger.getALLBurgers());
+    });
+
+    app.post("/api/add",(req,res)=>{
+        let burgerName = req.body.name;
+        res.send(burger.addBurger(burgerName));
+    });
+    
+    app.put("/api/burgers/:id",(req,res)=>{
+        let burgerID = req.params.id;
+        res.send(burger.eatBurger(burgerID));
+    });
+
+}
+
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// placeholder
+//imports
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+//set up server/express
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//set up handlebars
+app.engine("handlebars", handlebars({defaultLayout:"main"}));
+app.set("view engine","handlebars");
+
+//set up routes
+require("./controllers/burgersController")(app);
+
+//start the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
